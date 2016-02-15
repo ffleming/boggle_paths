f66e20309f40b8a12360aa8646523fff178a00ed
Forrest Fleming <ffleming@gmail.com>
2016-02-15 12:38:40 -0800

    add flag for forcing using GMP

--- main.h
+++ main.h
@@ -7,6 +7,6 @@
 #include <gmp.h>
 void help(char* program_name, int code);
 void output_bignums(int sides, bool quiet, int only_row, int only_col);
-void parse_opts(int argc, char** argv, bool*, int*, int*, int*);
+void parse_opts(int argc, char** argv, bool* quiet_flag, bool* bignum_flag, int* sides, int* only_row, int* only_col);
 void print_board(int sides);
 #endif
